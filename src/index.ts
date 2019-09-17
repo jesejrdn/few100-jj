70b249fa87c4d36f9bfa1ba06f14991f9faa6da1
jesejrdn <32983329+jesejrdn@users.noreply.github.com>
2019-09-17 19:51:10 +0000

    Created add utility

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,12 @@
-
 import './styles.css';
-console.log('Ready to Party With Some TypeScript!');
+import { add } from './math';
 
+const submitButton = document.getElementById('sum') as HTMLButtonElement;
+const num1 = document.getElementById('num1') as HTMLInputElement;
+const num2 = document.getElementById('num2') as HTMLInputElement;
+const answer = document.getElementById('answer') as HTMLSpanElement;
+
+submitButton.addEventListener('click', () => {
+    const sum = add(num1.valueAsNumber, num2.valueAsNumber);
+    answer.innerText = sum.toString();
+});
